f25f2da52d637bca20fde78978def85674c42b0f
Matt Foster <fmatthew40@gmail.com>
2021-11-12 10:54:24 -0700

    update potion.js

--- jest-another-RPG/lib/Potion.js
+++ jest-another-RPG/lib/Potion.js
@@ -2,7 +2,6 @@ class Potion {
   constructor(name) {
     this.types = ['strength', 'agility', 'health'];
     this.name = name || this.types[Math.floor(Math.random() * this.types.length)];
-
     if (this.name === 'health') {
       this.value = Math.floor(Math.random() * 10 + 30);
     } else {
